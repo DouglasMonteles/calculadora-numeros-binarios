2b2bf8bcc984ee7d09d61770f4112ced6668c8fa
DouglasMonteles <silvadouglas328@gmail.com>
2020-11-06 01:16:59 -0300

    Criando o componente Footer

--- calculadora-web/src/app/app.module.ts
+++ calculadora-web/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
     LandingPageComponent,
     DashboardComponent,
     HeaderComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +32,7 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
